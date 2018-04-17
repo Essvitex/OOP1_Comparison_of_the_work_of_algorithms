6ccc393b43e59a6356b8ff644e16727c3313f5e4
Victor Sergeev <38391733+Essvitex@users.noreply.github.com>
2018-04-17 19:52:27 +0500

    Parsing parameters into spaces

---
+++ Parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FirstLab
+{
+    /// <summary>
+    /// Разбиение строки по пробелам, формирование списка параметров команды
+    /// </summary>
+    public static class Parser
+    {
+        /// <summary>
+        /// Разбиение строки по пробелам, формирование списка параметров команды
+        /// </summary>
+        /// <param name="str">строка команды для парсинга</param>
+        /// <returns>
+        /// возвращает false если пустая строка, в таком случае завершается
+        /// работа программы
+        /// </returns>
+        public static Command ParseComm(string str)
+        {
+            try
+            {
+                var currListComm =
+                    str.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                if (currListComm.Length == 0)
+                {
+                    return null;
+                }
+
+                var param = new List<int>();
+
+                for (var i = 1; i < currListComm.Length; i++)
+                {
+                    int currParam;
+                    if (currListComm[i].Select(ch => !char.IsLetter(ch)).Any(flag => !flag) 
+                        || !int.TryParse(currListComm[i], out currParam))
+                    {
+                        throw new InvalidDataException("символ '" + currListComm[i] + 
+                                                       "', позиция " + i);
+                    }
+                    param.Add(currParam);
+                }
+                return new Command(currListComm[0], param);
+            }
+            catch (InvalidDataException e)
+            {
+                Console.WriteLine("Параметр должен содержать только целые числа " + e.Message);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("Ошибка при парсинге: {0}", x);
+            }
+            return null;
+        }
+    }
+}
