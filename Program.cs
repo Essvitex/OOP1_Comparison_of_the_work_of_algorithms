1a74ebf7658c753c6ea41c757d7ffb99177835b1
Victor Sergeev <38391733+Essvitex@users.noreply.github.com>
2018-04-17 19:45:44 +0500

    Main file

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+/* Лабораторная работа №1: Сравнение работы алгоритмов.
+ * Исполнители: Зотов Антон, Сергеев Виктор. Группа: ФО - 260002
+ */
+namespace FirstLab
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Если задан параметр - имя текстового файла, то открываем поток на чтение
+            // из этого файла и прередаем его в конструктор.
+            var reader = args.Length > 0 ? File.OpenText(args[0]) : Console.In;
+            using (reader)
+            {
+                var robot = new Processing(reader, args.Length == 0);
+                robot.Run();
+            }
+        }
+    }
+}
